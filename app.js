0a8bdbe56ff8737bb7c7f73c6357080852024357
Yevhenii Kolomiiets <yevheniikolomiiets@gmail.com>
2021-01-09 12:02:47 +0200

    Add prettier

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const port = process.env.APP_PORT
+const express = require('express');
+const app = express();
+const port = process.env.APP_PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
+const { Pool, Client } = require('pg');
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'dictionary',
+  password: 'admin',
+  port: 5432,
+});
 
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
 
-const { Pool, Client } = require('pg')
-  const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'dictionary',
-    password: 'admin',
-    port: 5432,
-  })
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'dictionary',
+  password: 'admin',
+  port: 5432,
+});
 
-  pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
+client.connect();
 
-  const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'dictionary',
-    password: 'admin',
-    port: 5432,
-  })
-
-  client.connect()
-
-
-  client.query('SELECT * FROM users', (err, res) => {
-    console.log(res.rows)
-    client.end()
-  })
-
-
+client.query('SELECT * FROM users', (err, res) => {
+  console.log(res.rows);
+  client.end();
+});
